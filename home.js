b6e03c0e7e995a01fd2043ccd77d2f653713c675
Alex Faulkner <alex.w.faulkner13@gmail.com>
2022-06-27 19:08:12 +0100

    improved de-glowification

--- home.js
+++ home.js
@@ -21,6 +21,21 @@ window.onload = () => {
     if (mapType) slMap.value = mapType;
     if (screenType) slScreen.value = screenType;
     if (unitType) slUnitType.value = unitType;
+
+    if (!(navigator.doNotTrack == "1" || navigator.doNotTrack == "yes" || window.doNotTrack == "1"))
+    {
+        var glowScript = document.createElement("script");
+        glowScript.src = "https://www.googletagmanager.com/gtag/js?id=G-LB4FV46CJK";
+        glowScript.async = true;
+
+        document.body.appendChild(glowScript);
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LB4FV46CJK');
+    }
 };
 
 function toggleOptions()
